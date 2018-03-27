ce9a1e733bc4e1a9dc580587d3d49bc22faba450
DuyDo <duydo.dev@gmail.com>
2018-03-27 01:56:20 -0500

    call ajax in main.js sucess

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 var username = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-var chosen;
-var xhr = new XMLHttpRequest();
-var queryURL = "https://wind-bow.gomix.me/twitch-api/streams/" + chosen +"?callback=?";
-xhr.open("GET", queryURL, true);
-xhr.send(null);
-xhr.onload = function() {
-    if(xhr.status === 200) {
-        console.log()
-    }
-}
+var all = "";
+var live = "";
+var offline = "";
+username.forEach(function(elem){
+var queryURL = "https://wind-bow.gomix.me/twitch-api/streams/" + elem +"?callback=?";
+$.ajax({
+        url: queryURL,
+        crossDomain: true,
+        dataType: "jsonp",
+        method: "GET"
+    }).done(function(response){
+        console.log(response);
+        if(response.stream != null) {
+            live += "<p>" + elem +"</p>";
+        } else {
+            offline += "<p>" + elem + "</p>";
+        }
+
+        all = live + offline;
+    })
+})
+
+$("#live").on("click", function(){
+    $("#content").html(live);
+});
+
+$("#offline").on("click", function(){
+    $("#content").html(offline);
+});
+
+$("#all").on("click", function(){
+    $("#content").html(all);
+})
